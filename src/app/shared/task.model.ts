038ef4ee50fdaed45b01fb258daa6c7a0a53abe6
ghassenbns <ghassenbenslimane00@gmail.com>
2021-12-04 20:53:06 +0100

    Done to task

--- src/app/shared/task.model.ts
+++ src/app/shared/task.model.ts
@@ -1,9 +1,11 @@
 export class task {
   public title: string;
   public time: string;
+  public done: boolean;
 
-  constructor(title: string, time: string) {
+  constructor(title: string, time: string, done: boolean) {
     this.title = title;
     this.time = time;
+    this.done = done;
   }
 }
