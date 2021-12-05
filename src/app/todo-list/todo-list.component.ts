45bdf7d69083908179f893466619b4cc128906b8
Rim-Fekih <52582108+rim-fekih@users.noreply.github.com>
2021-12-06 00:32:40 +0100

    getAllTasks+stateChanging

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -6,12 +6,15 @@ import { AngularFireDatabase } from '@angular/fire/compat/database';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
+  addTask = true;
   currentDate: string;
   myTask = '';
-  addTask: boolean;
+  tasks = [];
+
   constructor(public afDB: AngularFireDatabase) {
     const date = new Date();
     this.currentDate = date.toLocaleDateString();
+    this.getTasks();
   }
 
   ngOnInit() {}
@@ -19,6 +22,10 @@ export class TodoListComponent implements OnInit {
     this.afDB.list('User/').push({
       pseudo: 'rim',
     });
+  }
+  showForm() {
+    this.addTask = !this.addTask;
+    this.myTask = '';
   }
   addTaskToFirebase() {
     this.afDB.list('Tasks/').push({
@@ -28,8 +35,28 @@ export class TodoListComponent implements OnInit {
     });
     this.showForm();
   }
-  showForm() {
-    this.addTask = !this.addTask;
-    this.myTask = '';
+
+  getTasks() {
+    this.afDB
+      .list('Tasks/')
+      .snapshotChanges(['child_added', 'child_removed'])
+      .subscribe((actions) => {
+        this.tasks = [];
+        actions.forEach((action) => {
+          this.tasks.push({
+            key: action.key,
+            text: action.payload.exportVal().text,
+            hour: action.payload.exportVal().date.substring(11, 16),
+            checked: action.payload.exportVal().checked,
+          });
+        });
+      });
+  }
+  changeCheckState(ev: any) {
+    console.log('checked: ' + ev.checked);
+    this.afDB.object('Tasks/' + ev.key + '/checked/').set(ev.checked);
+  }
+  deleteTask(task: any) {
+    this.afDB.list('Tasks/').remove(task.key);
   }
 }
